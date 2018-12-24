c61c24ef8532fec901f5fb781d315b5caa4b4afb
Danila <danilamastergrigorenko@gmail.com>
2018-12-24 15:16:10 +0300

    safe null

--- app/src/main/java/com/example/master/githubusers/users/presentation/UsersAdapter.kt
+++ app/src/main/java/com/example/master/githubusers/users/presentation/UsersAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.master.githubusers.R
 import com.example.master.githubusers.users.domain.model.User
 import kotlinx.android.synthetic.main.item_user.view.*
@@ -29,11 +30,11 @@ class UsersAdapter(val items : ArrayList<User>, val context: Context) : Recycler
 
     // Binds each animal in the ArrayList to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvName?.text = items.get(position).login
+        holder.tvName.text = items[position].login ?: "Empty login"
     }
 }
 
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     // Holds the TextView that will add each animal to
-    val tvName = view.iu_name
+    val tvName: TextView = view.iu_name
 }
